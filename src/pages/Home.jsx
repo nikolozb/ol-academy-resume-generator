5dd08d69df84614caecfb08a2254fa8365c1e45f
nikolozb <berose995@gmail.com>
2022-11-17 14:37:44 +0400

    home-page/latest resume block

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,19 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <Link to="/choose-template">Choose Template</Link>
-      <Link to="/import-resume">Import Resume</Link>
-      <Link to="/">Continue</Link>
+    <div className="home">
+      <div className="home__last">
+        <h4 className="home__title">Latest resume:</h4>
+        <span className="home__info">name lastname | date</span>
+        <Link to="/stored_resumes" className="home__stored">
+          See all
+        </Link>
+      </div>
+      <div>
+        <Link to="/choose-template">Choose Template</Link>
+        <Link to="/import-resume">Import Resume</Link>
+        <Link to="/">Continue</Link>
+      </div>
     </div>
   );
 };
