bc2c6d22c4d65641df58ec7d80b04a6ed2b299a0
nikolozb <berose995@gmail.com>
2022-11-17 14:03:43 +0400

    import-resume-page/file upload

--- src/pages/ImportResume.jsx
+++ src/pages/ImportResume.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
 const ImportResume = () => {
-  return <div>ImportResume</div>;
+  const [data, setData] = useState(null);
+
+  const handleUpload = async (e) => {
+    const reader = new FileReader();
+
+    reader.onload = async (e) => {
+      const text = e.target.result;
+      setData(JSON.parse(text));
+    };
+
+    reader.readAsText(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <label htmlFor="file" className="custom-file-upload">
+        Upload a file
+      </label>
+      <input
+        id="file"
+        type="file"
+        accept="application/JSON"
+        onChange={handleUpload}
+      />
+    </>
+  );
 };
 
 export default ImportResume;
