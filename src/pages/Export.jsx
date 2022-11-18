180ce23443b0765bb9648a8c0fdb5837b9041a64
nikolozb <berose995@gmail.com>
2022-11-18 16:24:29 +0400

    export-page/export pdf

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { colorPicker } from "../helpers/colorPicker";
+import { useReactToPrint } from "react-to-print";
 
 import AngoraTemplate from "../components/AngoraTemplate";
 import BlueprintTemplate from "../components/BlueprintTemplate";
@@ -13,16 +14,24 @@ const Export = () => {
   const [
     { name, email, address, number, education, experience, skills, prosummary },
   ] = useState(JSON.parse(localStorage.getItem("data")));
+  const resumeRef = useRef();
+
+  const printPdfHandler = useReactToPrint({
+    documentTitle: "cv-pdf",
+    content: () => resumeRef.current,
+  });
 
   return (
     <div className="export">
       <div className="export__buttons">
         <Button size="lg">Export Image</Button>
-        <Button size="lg">Export PDF</Button>
+        <Button size="lg" onClick={printPdfHandler}>
+          Export PDF
+        </Button>
         <ExportJSON />
         <Button size="lg">Build another resume</Button>
       </div>
-      <div className="export__preview">
+      <div className="export__preview" ref={resumeRef}>
         {theme === "Angora" && (
           <AngoraTemplate
             name={name}
