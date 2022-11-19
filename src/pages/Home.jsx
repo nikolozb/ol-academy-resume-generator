e7b2d6f1b1ed67d2d82408bd2e24f4444da84cc6
nikolozb <berose995@gmail.com>
2022-11-19 21:30:57 +0400

    home-page/fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,31 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 import { Button } from "reactstrap";
 
 const Home = () => {
-  const AllResumes = JSON.parse(localStorage.getItem("all_resumes"));
-  const lastResume =
-    AllResumes === undefined || AllResumes === null
-      ? {}
-      : AllResumes[AllResumes.length - 1];
+  const [lastResume, setLastResume] = useState(null);
+
+  useEffect(() => {
+    const AllResumes = JSON.parse(localStorage.getItem("all_resumes"));
+    if (AllResumes?.length !== 0 && AllResumes !== null) {
+      setLastResume(AllResumes[AllResumes.length - 1]);
+    } else return;
+  }, []);
+
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate({
+      pathname: "/create",
+    });
+  };
 
   return (
     <div className="home">
       <div className="home__last">
         <h4 className="home__title">Latest resume:</h4>
         <span className="home__info">
-          {AllResumes === null ? (
+          {lastResume === null ? (
             <h3>No resumes</h3>
           ) : (
             <span>
