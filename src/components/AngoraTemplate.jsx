ce2d2e554502bd48dd3b8a18e58dd3d6080ac05a
nikolozb <berose995@gmail.com>
2022-11-23 15:14:48 +0400

    fix

--- src/components/AngoraTemplate.jsx
+++ src/components/AngoraTemplate.jsx
@@ -13,50 +13,35 @@ const AngoraTemplate = ({
 }) => {
   return (
     <div className="resume-template angora">
+      <h1 className="angora__name" style={{ color: `${color}` }}>
+        {name}
+        <hr />
+      </h1>
       <div className="angora__contacts">
         <h5 className="angora__contacts-item">Address: {address}</h5>
         <h5 className="angora__contacts-item">Email: {email}</h5>
         <h5 className="angora__contacts-item">Number: {number}</h5>
       </div>
+      <h1 style={{ color: `${color}` }}>
+        Education
+        <hr />
+      </h1>
       <div className="angora__edu">
-        <h2 style={{ color: `${color}` }}>
-          Education
-          <hr />
-        </h2>
         <div className="angora__edu-content flex-between">
           <p>{education}</p>
         </div>
       </div>
+      <h1 style={{ color: `${color}` }}>
+        Experience
+        <hr />
+      </h1>
       <div className="angora__exp">
-        <h2 style={{ color: `${color}` }}>
-          Experience
-          <hr />
-        </h2>
         <div className="angora__exp-content">
           <div className="flex-between">
-
-            <span>{experience}</span>
-          </div>
-        </div>
-      </div>
-      <div className="angora__skills">
-        <h2 style={{ color: `${color}` }}>
-          Skills
-          <hr />
-        </h2>
-        {skills}
-      </div>
-      <div className="angora__prosum">
-        <h2 style={{ color: `${color}` }}>
-          Professional Summary
-          <hr />
-        </h2>
-        {prosummary}
             <p>{experience}</p>
           </div>
         </div>
       </div>
-  
       <h1 style={{ color: `${color}` }}>
         Skills
         <hr />
